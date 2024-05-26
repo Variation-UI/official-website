c4687918abd61eda5ec4f6b9f4541b67aed699a9
useless-anlong <3618542587@qq.com>
2024-05-26 20:00:09 +0800

    Get: Add Opkona device page and Info json files (Not Completed)

--- script/app.js
+++ script/app.js
@@ -8,63 +8,63 @@ const CARDS = [
   {
     name: "Discord",
     icon: "ri-discord-fill",
-    link: "https://discord.com/app",
+    link: "./get/opkona.html",
     color: "#5865F2",
   },
-  {
-    name: "Reddit",
-    icon: "ri-reddit-fill",
-    link: "https://www.reddit.com/",
-    color: "#FF4500",
-  },
-  {
-    name: "Figma",
-    icon: "ri-pen-nib-fill",
-    link: "https://www.figma.com/",
-  },
-  {
-    name: "Github",
-    icon: "ri-github-fill",
-    link: "https://github.com/",
-  },
-  {
-    name: "Twitter",
-    icon: "ri-twitter-fill",
-    link: "https://twitter.com",
-    color: "#1DA1F2",
-  },
-  {
-    name: "Dribbble",
-    icon: "ri-dribbble-fill",
-    link: "https://dribbble.com/",
-    color: "#ea4c89",
-  },
-  {
-    name: "Hashnode",
-    icon: "ri-newspaper-line",
-    link: "https://hashnode.com/",
-  },
-  {
-    name: "CodeSandbox",
-    icon: "ri-braces-fill",
-    link: "https://codesandbox.io/dashboard/",
-  },
-  {
-    name: "YouTube",
-    icon: "ri-youtube-fill",
-    link: "https://www.youtube.com/",
-    color: "#FF0000",
-  },
-  {
-    name: "LinkedIn",
-    icon: "ri-linkedin-fill",
-    link: "https://www.linkedin.com/",
-  },
-  {
-    name: "Gmail",
-    icon: "ri-google-fill",
-    link: "https://mail.google.com/",
-  },
+  // {
+  //   name: "Reddit",
+  //   icon: "ri-reddit-fill",
+  //   link: "https://www.reddit.com/",
+  //   color: "#FF4500",
+  // },
+  // {
+  //   name: "Figma",
+  //   icon: "ri-pen-nib-fill",
+  //   link: "https://www.figma.com/",
+  // },
+  // {
+  //   name: "Github",
+  //   icon: "ri-github-fill",
+  //   link: "https://github.com/",
+  // },
+  // {
+  //   name: "Twitter",
+  //   icon: "ri-twitter-fill",
+  //   link: "https://twitter.com",
+  //   color: "#1DA1F2",
+  // },
+  // {
+  //   name: "Dribbble",
+  //   icon: "ri-dribbble-fill",
+  //   link: "https://dribbble.com/",
+  //   color: "#ea4c89",
+  // },
+  // {
+  //   name: "Hashnode",
+  //   icon: "ri-newspaper-line",
+  //   link: "https://hashnode.com/",
+  // },
+  // {
+  //   name: "CodeSandbox",
+  //   icon: "ri-braces-fill",
+  //   link: "https://codesandbox.io/dashboard/",
+  // },
+  // {
+  //   name: "YouTube",
+  //   icon: "ri-youtube-fill",
+  //   link: "https://www.youtube.com/",
+  //   color: "#FF0000",
+  // },
+  // {
+  //   name: "LinkedIn",
+  //   icon: "ri-linkedin-fill",
+  //   link: "https://www.linkedin.com/",
+  // },
+  // {
+  //   name: "Gmail",
+  //   icon: "ri-google-fill",
+  //   link: "https://mail.google.com/",
+  // },
 ];
 
 /* -------------------------------------------------------- */
@@ -129,10 +129,10 @@ const printCards = () => {
     currentCard.href = card.link;
 
     // Style the Icon
-    currentCardIcon.classList.add("card__icon");
+    currentCardIcon.classList.add("card_icon");
 
     // Style the Text
-    currentCardText.classList.add("card__name");
+    currentCardText.classList.add("card_name");
 
     currentCard.append(currentCardIcon);
     currentCard.append(currentCardText);
@@ -175,4 +175,3 @@ const printCards = () => {
 /****************/
 
 printCards();
-updateDate();
